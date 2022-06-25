c430df57234096b56e7ef018f6bc5f48a659c0cc
Dennis Aumiller <dennis.aumiller@gmx.de>
2022-06-25 02:06:09 +0200

    Script to get sample generated summaries.

---
+++ Baselines/extract_sample_files_summaries.py
@@ -0,0 +1,43 @@
+"""
+Load the samples that are included in the submission
+"""
+import os
+import json
+import shutil
+
+from lexrank_baselines import clean_celex_id
+
+if __name__ == '__main__':
+    with open("../Analysis/sample_data.json") as f:
+        data = json.load(f)
+
+    for language, all_data in data.items():
+        for split, samples in all_data.items():
+            # TODO: We could arguably fine-tune on this data?
+            if split == "train":
+                continue
+            else:
+                # for method in ["paragraph", "oracle", "lexrank",
+                #                "./cross_lingual_baselines/translated"]:
+                #     if method != "paragraph" and language != "spanish":
+                #         continue
+                #     if method == ""
+
+                out_path = os.path.join("./generated_samples/", "paragraph", language, split)
+                os.makedirs(out_path, exist_ok=True)
+                for celex_id, _ in samples.items():
+                    in_file = os.path.join("./paragraph", language, split, f"{clean_celex_id(celex_id)}.txt")
+                    out_file = os.path.join(out_path, f"{clean_celex_id(celex_id)}.txt")
+                    shutil.copyfile(in_file, out_file)
+
+                    if language == "spanish":
+                        print(f"{split}, {celex_id}")
+
+                if language == "spanish":
+                    for method in ["oracle", "translated", "lexrank"]:
+                        out_path = os.path.join("./generated_samples/", "oracle", language, split)
+                        os.makedirs(out_path, exist_ok=True)
+                        for celex_id, _ in samples.items():
+                            in_file = os.path.join("./cross_lingual_baselines/", method, language, split, f"{celex_id}.txt")
+                            out_file = os.path.join(out_path, f"{clean_celex_id(celex_id)}.txt")
+                            shutil.copyfile(in_file, out_file)
