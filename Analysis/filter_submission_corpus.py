6ef5bc9f8600428c4b41c0d4ebc6306bd26d5800
Dennis Aumiller <dennis.aumiller@gmx.de>
2022-06-25 04:46:51 +0200

    Fixing extraction script.

--- Analysis/filter_submission_corpus.py
+++ Analysis/filter_submission_corpus.py
@@ -5,25 +5,27 @@ such that we can submit it.
 
 import json
 import pickle
-from itertools import islice
 
 from tqdm import tqdm
 
 
-def take(n, iterable):
-    "Return first n items of the iterable as a list"
-    return list(islice(iterable, n))
+def take(n, dict_items):
+    d = {}
+    for idx, (k, v) in enumerate(dict_items):
+        if idx >= n:
+            return d
+        d[k] = v
 
 
 if __name__ == '__main__':
     with open("clean_data.pkl", "rb") as f:
         data = pickle.load(f)
 
-    no_per_set = 5
+    no_per_set = 2
 
     for language, all_data in tqdm(data.items()):
         for split, samples in all_data.items():
-            data[language][split] = take(2, samples.items())
+            data[language][split] = take(no_per_set, samples.items())
 
     with open("sample_data.json", "w") as f:
-        json.dump(data, f, indent=2)
+        json.dump(data, f, indent=2, ensure_ascii=False)
